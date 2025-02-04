44479fd70d0d5f015451718b680865720dcfe1fb
RishabChoudhary75 <rishab.choudhary.hp@gmail.com>
2025-02-04 11:55:47 +0530

    v2

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json());
-
+// app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
 const PORT = process.env.PORT || 3000;
 
 // Database connection
